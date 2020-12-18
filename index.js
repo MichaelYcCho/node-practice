0f64173b510da82b95337931845a891ca13505c9
Youngchan <michael_cho77@naver.com>
2020-12-19 01:27:44 +0900

    Handling Routes with Express

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+
+const handleHome = (req, res) => {
+    res.send("Home");
+}
+
+
+const handleProfile = (req, res) => {
+    res.send("You are on my profile");
+}
+
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+
+app.listen(PORT, handleListening);
