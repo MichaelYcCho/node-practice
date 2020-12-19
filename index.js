ba8a2a9848bcf0e72e1339fa64c5d795d2cf946e
Youngchan <michael_cho77@naver.com>
2020-12-19 23:36:09 +0900

    Express Core: Middlewares

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 //const express = require("express");
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 
 const app = express();
@@ -20,6 +24,11 @@ const handleProfile = (req, res) => {
     res.send("You are on my profile");
 }
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
