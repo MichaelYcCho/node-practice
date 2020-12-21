747493c528f305781b68995f77f6275bda9e63b9
Youngchan <michael_cho77@naver.com>
2020-12-21 23:22:14 +0900

    Installing Pug

--- app.js
+++ app.js
@@ -9,14 +9,25 @@ import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 const app = express();
 
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
-app.use(morgan("dev"));
+app.set("view engine", "pug");
 
+// 미들웨어영역
+app.use(helmet()); // 보안강화를 위해 미들웨어에 같이 쓰임
+app.use(cookieParser()); // 쿠키를 전달받아서 사용할수 있도록 하는 미들웨어 사용자인증시 필요
+app.use(bodyParser.json()); // 사용자가 웹사이트로 전달하는 정보들을 검사함  request정보에서 form이나 json형태로된 body를 검사함
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+/*
+ morgan
+ tiny -어떤접속(get)인지, 어디서 접속했는지 알수 있음 / combine - 어떤종류의 브라우저인지, 어떤종류의 접속인지 알 수 있음
+ 어플리케이션에서 발생하는 모든것을 로깅(logging) 함
+*/
+
+
+// 라우터영역
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
+
 
 export default app;
