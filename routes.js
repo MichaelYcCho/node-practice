59628f01eb194e459789a5af795e187cfb789909
Youngchan <michael_cho77@naver.com>
2020-12-31 17:07:24 +0900

    Github LogIn

--- routes.js
+++ routes.js
@@ -6,19 +6,21 @@ const LOGOUT = "/logout";
 const SEARCH = "/search";
 
 // Users
-
 const USERS = "/users";
 const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
 
 // Videos
-
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
+
+// Github
+const GITHUB = "/auth/github";
+const GITHUB_CALLBACK = "/auth/github/callback";
 
 const routes = {
     home: HOME,
@@ -58,7 +60,9 @@ const routes = {
         } else {
             return DELETE_VIDEO;
         }
-    }
+    },
+    gitHub: GITHUB,
+    githubCallback: GITHUB_CALLBACK
 };
 
 export default routes;
