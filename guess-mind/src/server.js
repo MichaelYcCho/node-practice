08349aa5086607f1ed8bca8d95dc8a8cfbf0cf92
Youngchan <michael_cho77@naver.com>
2021-01-04 23:29:45 +0900

    Chat with SocketIO

--- guess-mind/src/server.js
+++ guess-mind/src/server.js
@@ -20,7 +20,15 @@ const server = app.listen(PORT, handleListening);
 const io = socketIO(server);
 
 io.on("connection", socket => {
-    socket.on("helloGuys", () => console.log("the client said hello"));
+    socket.on("newMessage", ({ message }) => {
+        socket.broadcast.emit("messageNotif", {
+            message,
+            nickname: socket.nickname || "YC"
+        });
+    });
+    socket.on("setNickname", ({ nickname }) => {
+        socket.nickname = nickname;
+    })
 })
 
 //setInterval(() => console.log(sockets), 1000);
--- guess-mind/src/static/index.js
+++ guess-mind/src/static/index.js
@@ -1,6 +1,18 @@
+// eslint-disable-next-line no-undef
 const socket = io("/");
 
-socket.on("hello", () => console.log("Somebody said hello"));
+function sendMessage(message) {
+    socket.emit("newMessage", { message });
+    console.log(`You: ${message}`);
+}
 
+function setNickname(nickname) {
+    socket.emit("setNickname", { nickname });
+}
 
-setTimeout(() => socket.emit("helloGuys"), 4000);
+function handleMessageNotif(data) {
+    const { message, nickname } = data;
+    console.log(`${nickname}: ${message}`);
+}
+
+socket.on("messageNotif", handleMessageNotif);
