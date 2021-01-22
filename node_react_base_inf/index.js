b3b36ba1a99bfc9d47eb21b74d1ad08b8d767185
Youngchan <michael_cho77@naver.com>
2021-01-22 23:50:18 +0900

    (NRB) - Register with postman

--- node_react_base_inf/index.js
+++ node_react_base_inf/index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const dotenv = require('dotenv')
 const app = express()
 const port = process.env.PORT || 4000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded 데이터를 분석해서 가져옴
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json 을 분석
+app.use(bodyParser.json());
 
 dotenv.config()
 
@@ -14,5 +22,20 @@ mongoose.connect(`mongodb+srv://michael:${process.env.DB_Password}@cluster0.donh
 
 
 app.get('/', (req, res) => res.send('Hello World!~~안녕하세요 ~ '))
+
+app.post('/register', (req, res) => {
+
+    // client에서 회원가입(User model)정보를 받아 db에 넣는다
+
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
