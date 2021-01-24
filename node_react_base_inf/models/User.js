3d1c100edd3453d6f3e22df8d9b1808364838b40
Youngchan <michael_cho77@naver.com>
2021-01-24 21:05:07 +0900

    (NRB) - Bcrypt를 활용한 암호변경

--- node_react_base_inf/models/User.js
+++ node_react_base_inf/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -32,5 +35,24 @@ const userSchema = mongoose.Schema({
 })
 
 const User = mongoose.model('User', userSchema)
+
+userSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        // salt생성
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+        })
+
+        //비밀번호의 암호화
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err)
+
+            user.password = hash
+            next()
+        })
+    }
+})
 
 module.exports = { User }
