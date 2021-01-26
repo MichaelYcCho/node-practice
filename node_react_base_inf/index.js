004988b08d0f9d8d181bf2b89b170fb69ab4115f
Youngchan <michael_cho77@naver.com>
2021-01-26 23:50:55 +0900

    (NRB) - Authentication

--- node_react_base_inf/index.js
+++ node_react_base_inf/index.js
@@ -4,6 +4,7 @@ const app = express()
 const port = process.env.PORT || 4000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 
 const config = require('./config/key');
@@ -70,6 +71,20 @@ app.post('/api/users/login', (req, res) => {
     })
 })
 
+app.get('/api/users/auth', auth, (req, res) => {
+
+    //0 : 일반유저 아니면 관리자일때
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
