f82d3f847f35827237f111c5c5078e65a4259055
Youngchan <michael_cho77@naver.com>
2021-02-27 01:49:22 +0900

    POC API

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 import { ApolloServer, gql } from "apollo-server";
 
-
 const typeDefs = gql`
+    type Movie{
+        title: String
+        yaer: Int
+    }
     type Query{
-        hello:String
+        movies: [Movie]
+        movie: Movie
+    }
+    type Mutation{
+        createMovie(title: String!): Boolean
+        deleteMovie(title: String!): Boolean
     }
 `;
 
 const resolvers = {
     Query: {
-        hello: () => "worlds",
+        movies: () => [],
+        movie: () => ({ "title": "Hello", year: 2021 })
     },
+    Mutation: {
+        createMovie: (_, { title }) => {
+            return true
+        },
+        deleteMovie: (_, { title }) => {
+            return true
+        }
+    }
 };
 
 const server = new ApolloServer({
