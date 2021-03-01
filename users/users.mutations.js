0b9b61ef92240df2ea67ab96cacae54a5dd70587
Youngchan <michael_cho77@naver.com>
2021-03-02 00:47:35 +0900

    Create Account part Two

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,9 +1,23 @@
+import client from "../client"
+
 export default {
     Mutation: {
-        createAccount: (_,
-            { firstName, lastName, username, email, password }
+        createAccount: async (_, { firstName, lastName, username, email, password }
         ) => {
             //check if username or email are already on DB,
+            const existingUser = await client.user.findFirst({
+                where: {
+                    OR: [
+                        {
+                            username,
+                        },
+                        {
+                            email,
+                        }
+                    ]
+                }
+            });
+            console.log(existingUser)
             //hash password
             //save and return the user
 
