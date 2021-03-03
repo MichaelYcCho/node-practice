919b514b73fa9f93b58be37247531d06b77d06dc
Youngchan <michael_cho77@naver.com>
2021-03-03 23:50:50 +0900

    seeProfile

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -5,32 +5,43 @@ export default {
     Mutation: {
         createAccount: async (_, { firstName, lastName, username, email, password }
         ) => {
-            //check if username or email are already on DB,
-            const existingUser = await client.user.findFirst({
-                where: {
-                    OR: [
-                        {
-                            username,
-                        },
-                        {
-                            email,
-                        }
-                    ]
+            try {
+                //check if username or email are already on DB,
+                const existingUser = await client.user.findFirst({
+                    where: {
+                        OR: [
+                            {
+                                username,
+                            },
+                            {
+                                email,
+                            }
+                        ]
+                    }
+                });
+                if (existingUser) {
+                    throw new Error("This username/email is already taken")
                 }
-            });
-            const uglyPassword = await bcrypt.hash(password, 10)
-            const user = await client.user.create({
-                data: {
-                    username,
-                    email,
-                    firstName,
-                    lastName,
-                    password: uglyPassword
-                }
-            })
-            return user
+                const uglyPassword = await bcrypt.hash(password, 10)
+                return client.user.create({
+                    data: {
+                        username,
+                        email,
+                        firstName,
+                        lastName,
+                        password: uglyPassword
+                    }
+                })
+
+            } catch (e) {
+                console.log(e)
+
+            }
+
             //hash password
             //save and return the user
+
         },
     }
 }
+
