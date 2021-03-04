fed2f053f4f4e61625d5581c820ce6f646641b62
Youngchan <michael_cho77@naver.com>
2021-03-04 23:55:34 +0900

    ADD - Error Handling AnotherWay

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -6,22 +6,21 @@ export default {
         createAccount: async (_, { firstName, lastName, username, email, password }
         ) => {
             try {
-                //check if username or email are already on DB,
-                const existingUser = await client.user.findFirst({
-                    where: {
-                        OR: [
-                            {
-                                username,
-                            },
-                            {
-                                email,
-                            }
-                        ]
-                    }
+                const existingUsername = await client.user.findFirst({
+                    where: { username },
                 });
-                if (existingUser) {
-                    throw new Error("This username/email is already taken")
+                const existingEmail = await client.user.findFirst({
+                    where: { email },
+                });
+                if (existingEmail && existingUsername) {
+                    throw new Error("Username and Email is alredy taken");
+                } else if (existingUsername) {
+                    throw new Error("Username is already taken");
+                } else if (existingEmail) {
+                    throw new Error("Email is already taken");
                 }
+                //check if username or email are already on DB,
+
                 const uglyPassword = await bcrypt.hash(password, 10)
                 return client.user.create({
                     data: {
@@ -45,3 +44,16 @@ export default {
     }
 }
 
+/* 또는 or을 사용해 아래처럼 구현가능
+const existingUser = await client.user.findFirst({
+    where: {
+        OR: [
+            { username, },
+            { email, }
+        ]
+    }
+});
+if (existingUser) {
+    throw new Error("This username/email is already taken")
+}
+*/
