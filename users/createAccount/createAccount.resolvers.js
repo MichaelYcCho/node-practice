72d5e8ca263f4ba04517afc85e06158751419ed1
Youngchan <michael_cho77@naver.com>
2021-03-09 00:05:55 +0900

    ADD - Apply Error

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,45 +1,41 @@
 import bcrypt from "bcrypt";
-import client from "../../client";
+import client from "../../client"
 
 export default {
     Mutation: {
-        createAccount: async (
-            _,
-            { firstName, lastName, username, email, password }
+        createAccount: async (_, { firstName, lastName, username, email, password }
         ) => {
-            // Check Username or Email is already taken
-            const existingUsername = await client.user.findUnique({
-                where: { username },
-            });
-            const existingEmail = await client.user.findUnique({ where: { email } });
-            if (existingEmail || existingUsername) {
-                return {
-                    success: false,
-                    error:
-                        existingEmail && existingUsername
-                            ? "Username and Email is already taken"
-                            : existingUsername
-                                ? "Username is already taken"
-                                : "Email is already taken",
-                };
+            try {
+                const existingUsername = await client.user.findUnique({
+                    where: { username },
+                });
+                const existingEmail = await client.user.findUnique({ where: { email } });
+                if (existingEmail && existingUsername) {
+                    throw new Error("Username and Email is alredy taken");
+                } else if (existingUsername) {
+                    throw new Error("Username is already taken");
+                } else if (existingEmail) {
+                    throw new Error("Email is already taken");
+                }
+                //check if username or email are already on DB,
+
+                const uglyPassword = await bcrypt.hash(password, 10)
+                return client.user.create({
+                    data: {
+                        username,
+                        email,
+                        firstName,
+                        lastName,
+                        password: uglyPassword
+                    }
+                })
+
+            } catch (e) {
+                console.log(e)
+                return e;
+
             }
-            // hashing password for security
-            const hashedPassword = await bcrypt.hash(password, 10);
-            // create User
-            const user = await client.user.create({
-                data: {
-                    firstName,
-                    lastName,
-                    username,
-                    email,
-                    password: hashedPassword,
-                },
-            });
-            // return User with ok
-            return {
-                success: true,
-                user,
-            };
         },
     },
 };
+
