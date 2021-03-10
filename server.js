ccac862a6c8df0d0a34d3eaa98be03821d6fdcf8
Youngchan <michael_cho77@naver.com>
2021-03-10 23:54:38 +0900

    ADD - Token Handeling on Header

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ import schema from "./schema";
 
 const server = new ApolloServer({
     schema,
+    context: {
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjE1MjE0OTA4fQ.ESB1UsflFIttMeszm_YlBjPjh-_gfqdnKnJthqKYiuA"
+    }
 });
 
 const PORT = process.env.PORT
