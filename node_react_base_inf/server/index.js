09ba6a31ed1aeae58488c7ab856fde5442cb9729
Youngchan <michael_cho77@naver.com>
2021-07-05 00:56:23 +0900

    ADD - Neatly Code

--- node_react_base_inf/server/index.js
+++ node_react_base_inf/server/index.js
@@ -3,7 +3,6 @@ const app = express()
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-const dotenv = require('dotenv')
 const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 
@@ -99,6 +98,6 @@ app.get('/api/users/logout', auth, (req, res) => {
         })
 })
 
-const port = 5000
-//const port = process.env.PORT || 5000
+
+const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
