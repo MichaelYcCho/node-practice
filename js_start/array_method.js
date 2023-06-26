946a09e7df7f6754a9f86f71fde9aa8873d1ff18
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-06-27 00:24:11 +0900

    feat: array map()

--- js_start/array_method.js
+++ js_start/array_method.js
@@ -12,5 +12,5 @@ for (let hobby of hobbies){ // python 의 for in 과 비슷
 } // Sports Cooking
 
 
-
-// 오 node array_method.js 로 실행가능 
+// map은 함수의 형태이며, 어떻게 배열 또는 배열의 엘리먼트를 변환할지 정의한다.
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby)); // [ 'Hobby: Sports', 'Hobby: Cooking' ]
