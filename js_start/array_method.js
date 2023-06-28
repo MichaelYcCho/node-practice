168353ca4a126dbb3650ad38b93fae737984eaf7
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-06-28 23:18:40 +0900

    feat: Spread 연산자

--- js_start/array_method.js
+++ js_start/array_method.js
@@ -7,6 +7,11 @@ const person = {
 };
 
 const hobbies = ['Sports', 'Cooking']; 
+
+const copiedArray = [...hobbies]; // spread operator
+console.log(copiedArray); // [ 'Sports', 'Cooking' ]
+
+
 // for (let hobby of hobbies){ // python 의 for in 과 비슷
 //     console.log(hobby);
 // } // Sports Cooking
@@ -15,5 +20,5 @@ const hobbies = ['Sports', 'Cooking'];
 
 // 참조 타입(array, function, object, date emd)은 배열이 저장된 메모리 위치를 가리키는 주소만 저장하므로, 새로운 원소를 추가한다고 해서 메모리 주소가 바뀌지 않는다.
 // 따라서 상수로 저장된값은 포인터로 나타낸 주소만 나타내고, 그 주소에 저장된 값은 변경이 가능하다.
-hobbies.push('Programming');
-console.log(hobbies); // [ 'Sports', 'Cooking', 'Programming' ]
+// hobbies.push('Programming');
+// console.log(hobbies); // [ 'Sports', 'Cooking', 'Programming' ]
