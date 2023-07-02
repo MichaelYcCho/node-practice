03e15b1914e265877579f9036cab868c275ff9ab
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-07-02 23:46:02 +0900

    feat: 요청 리디렉션

--- js_start/app.js
+++ js_start/app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 
 const server = http.createServer((req, res) => {
@@ -10,6 +11,14 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
         return res.end();
     }
+    if (url === '/message' && req.method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+
     console.log(req.url, req.method, req.headers);
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
