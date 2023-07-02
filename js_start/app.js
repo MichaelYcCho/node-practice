964211616b03ce3ba578e72d5a9dc57172bc885b
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-07-03 01:26:31 +0900

    feat: req Event 핸들러 샘플

--- js_start/app.js
+++ js_start/app.js
@@ -12,6 +12,20 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
     if (url === '/message' && req.method === 'POST') {
+        const body = [];
+        // on 메소드는 이벤트 리스너를 등록하는 메소드이다.(리스닝할 이벤트, 이벤트 발생시 실행할 콜백함수)
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            // Buffer.concat() 메소드는 주어진 Buffer 객체들을 하나로 합친다.
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+
         fs.writeFileSync('message.txt', 'DUMMY');
         res.statusCode = 302;
         res.setHeader('Location', '/');
