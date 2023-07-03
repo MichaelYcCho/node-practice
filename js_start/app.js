e643c103276b8a459d7b61999d5c4e90869ccfa2
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-07-04 01:16:31 +0900

    feat: 블록킹 적용 샘플

--- js_start/app.js
+++ js_start/app.js
@@ -23,13 +23,15 @@ const server = http.createServer((req, res) => {
             const parsedBody = Buffer.concat(body).toString();
             console.log(parsedBody);
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
+            fs.writeFileSync('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         });
 
-        fs.writeFileSync('message.txt', 'DUMMY');
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+
+
     }
 
 
