05b21401aaf7efc2e6ba5cf250a6ab36fd1f0175
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-07-05 00:00:11 +0900

    feat: Router

--- js_start/app.js
+++ js_start/app.js
@@ -1,46 +1,9 @@
 const http = require('http');
-const fs = require('fs');
 
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>My First Page</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if (url === '/message' && req.method === 'POST') {
-        const body = [];
-        // on 메소드는 이벤트 리스너를 등록하는 메소드이다.(리스닝할 이벤트, 이벤트 발생시 실행할 콜백함수)
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            // Buffer.concat() 메소드는 주어진 Buffer 객체들을 하나로 합친다.
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
+console.log(routes.someText);
 
-
-
-    }
-
-
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
