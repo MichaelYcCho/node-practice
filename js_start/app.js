d26b21f84fe69c528afc916a1e2cc2c886757a9d
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-07-08 16:00:11 +0900

    feat: Express 세팅

--- js_start/app.js
+++ js_start/app.js
@@ -1,9 +1,9 @@
 const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
 
-const server = http.createServer(routes.handler);
+const server = http.createServer(app);
 
 server.listen(3000);
