0e32e6bd9696b530fda62aeacc02dea72fac3759
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-07-09 23:29:16 +0900

    feat: Middle Sample

--- js_start/app.js
+++ js_start/app.js
@@ -3,6 +3,15 @@ const express = require('express');
 
 const app = express();
 
+// 새로운 미들웨어를 추가하는 방법
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('next middleware!');
+});
 
 const server = http.createServer(app);
 
