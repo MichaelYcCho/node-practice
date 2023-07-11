184f877f6cfcde4ae7592a0d9c5dd4bf73c6a9c0
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-07-11 23:00:11 +0900

    feat: MiddleWare Sample Case

--- js_start/app.js
+++ js_start/app.js
@@ -2,15 +2,23 @@ const express = require('express');
 
 const app = express();
 
-// 새로운 미들웨어를 추가하는 방법
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
+    next()
 });
 
-app.use((req, res, next) => {
+
+app.use('/add-product', (req, res, next) => {
+    console.log('another middleware!');
+    res.send('<h1>The Add Product Page!</h1>');
+});
+
+
+// 새로운 미들웨어를 추가하는 방법
+app.use('/', (req, res, next) => {
     console.log('next middleware!');
     res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
+
