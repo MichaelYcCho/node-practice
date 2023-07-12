c1cbf4c305685c8a93f357990aa7366f27525a73
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-07-13 00:02:15 +0900

    feat: Express Router Example

--- js_start/app.js
+++ js_start/app.js
@@ -2,27 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-
-app.use('/add-product', (req, res, next) => {
-    console.log('another middleware!'); res, next
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-
-// 새로운 미들웨어를 추가하는 방법
-app.use('/', (req, res, next) => {
-    console.log('next middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-});
 
 app.listen(3000);
 
