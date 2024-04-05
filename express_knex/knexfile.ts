2231a7022e2c06e69c08110e4f7c3657dc19f3e4
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-04-06 00:00:01 +0900

    feat: Migration 세팅

---
+++ express_knex/knexfile.ts
@@ -0,0 +1,74 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
+
+require('dotenv').config({path: './.env'});
+
+console.log('하', process.env.DB_CLIENT);
+
+
+
+module.exports = {
+
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './dev.sqlite3'
+  //   }
+  // },
+
+
+    client: process.env.DB_CLIENT,
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      port:process.env.DB_PORT,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './src/migrations',
+      tableName: 'knex_migrations'
+    }
+  }
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: process.env.DB_NAME,
+  //     user:     process.env.DB_USER,
+  //     password: process.env.DB_PASSWORD,
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: process.env.DB_NAME,
+  //     user:     process.env.DB_USER,
+  //     password: process.env.DB_PASSWORD,
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+
