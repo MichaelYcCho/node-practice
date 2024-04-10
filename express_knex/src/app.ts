5980b88e785c2c8f5a28f31eeb3847c7d0e1424e
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-04-11 00:11:34 +0900

    feat: Select Example

--- express_knex/src/app.ts
+++ express_knex/src/app.ts
@@ -1,26 +1,34 @@
 import express, { Application, Request, Response } from 'express'
 import knex from 'knex'
+require('dotenv').config({path: './.env'});
 
 const app: Application = express()
 
 const port: number = 3000
 
 app.get('/main', (req: Request, res: Response) => {
-    knex('users')
-    .select({
-      id: 'id',
-      username: 'name'
-    })
-    .then((users) => {
-      return res.json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      return res.json({success: false, message: 'An error occurred, please try again later.'});
-    })
-    res.send('Hello main')
-})
 
+    const knex = require('knex')({ 
+        client: 'pg',
+        connection:{
+            host: process.env.DB_HOST,
+            database: process.env.DB_NAME,
+            user:     process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            port:process.env.DB_PORT,
+        }})
+
+     knex.select('id').from('users').then((data: any) => {
+        console.log(data)
+
+    }
+    )
+    res.send('Hello World!')
+
+    
+   
+
+    })
 
 
 app.listen(port, function () {
