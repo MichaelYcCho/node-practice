f4d289a072076e304741b322d6e602b0a2b3251a
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-04-14 19:32:12 +0900

    feat: Post V1

---
+++ nest_inf/src/posts/posts.controller.ts
@@ -0,0 +1,60 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+  } from '@nestjs/common';
+  import { PostsService } from './posts.service';
+  
+  @Controller('posts')
+  export class PostsController {
+    constructor(private readonly postsService: PostsService) {}
+  
+    // 1) GET /posts
+    //    모든 posts를 다 가져온다.
+    @Get()
+    getPosts() {
+      return this.postsService.getAllPosts();
+    }
+  
+    // 2) GET /posts/:id
+    //    id에 해당하는 post를 가져온다.
+    //    예를 들어서 id=1일 경우 id가 1인 post를 가져온다.
+    @Get(':id')
+    getPost(@Param('id') id: string) {
+      return this.postsService.getPostById(+id);
+    }
+  
+    // 3) POST /posts
+    //    새로운 post를 생성한다.
+    @Post()
+    postPosts(
+      @Body('author') author: string,
+      @Body('title') title: string,
+      @Body('content') content: string,
+    ) {
+      return this.postsService.createPost(author, title, content);
+    }
+  
+    // 4) PATCH /posts/:id
+    //    id에 해당하는 post를 수정한다.
+    @Patch(':id')
+    patchPost(
+      @Param('id') id: string,
+      @Body('author') author?: string,
+      @Body('title') title?: string,
+      @Body('content') content?: string,
+    ) {
+      return this.postsService.updatePost(+id, author, title, content);
+    }
+  
+    // 5) DELETE /posts/:id
+    //    id에 해당하는 post를 삭제한다.
+    @Delete(':id')
+    deletePost(@Param('id') id: string) {
+      return this.postsService.deletePost(+id);
+    }
+  }
