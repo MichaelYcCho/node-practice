0b177120412482b89cd5e4625baa9d6a5672e9b1
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-05-01 00:11:11 +0900

    feat: authenticateWithEmailAndPassword

--- nest_inf/src/auth/auth.service.ts
+++ nest_inf/src/auth/auth.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersModel } from 'src/users/entities/users.entity';
 import { JWT_SECRET } from './const/auth.const';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
     constructor(
         private readonly jwtService: JwtService,
+        private readonly usersService: UsersService,
     ){}
 
     signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken : boolean){
@@ -19,6 +22,28 @@ export class AuthService {
             secret: JWT_SECRET,
             expiresIn: isRefreshToken ? '30d' : '15m',
         });  
+    }
+
+    async loginUser(user: Pick<UsersModel, 'email' | 'id'>){
+        const accessToken = this.signToken(user, false);
+        const refreshToken = this.signToken(user, true);
+        return {accessToken, refreshToken};
+    }
+
+    async authenticateWithEmailAndPassword(user: Pick<UsersModel, 'email' | 'password'>){
+        const existingUser = await this.usersService.getUserByEmail(user.email);
+
+        if(!existingUser){
+            throw new UnauthorizedException('Invalid credentials');
+        }
+        
+        const passOk = await bcrypt.compare(user.password, existingUser.password);
+
+        if(!passOk){
+            throw new UnauthorizedException('Invalid Password');
+        }
+
+        return this.loginUser(existingUser);
     }
 }
 
