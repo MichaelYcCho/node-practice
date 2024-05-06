dfd66c98eed048c39116dbc2c97659e5a19fcc2d
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-05-06 23:07:44 +0900

    feat: Decode Token, Controller

--- nest_inf/src/auth/auth.service.ts
+++ nest_inf/src/auth/auth.service.ts
@@ -62,7 +62,7 @@ export class AuthService {
         return this.loginUser(newUser);
     }
 
-    async extractTokenFromHeader(header: string, isBearer : boolean){
+    extractTokenFromHeader(header: string, isBearer : boolean){
         const splitToken = header.split(' ');
         const prefix = isBearer ? 'Bearer' : 'Basic';
 
@@ -71,7 +71,24 @@ export class AuthService {
         }
 
         const token = splitToken[1];
+        
         return token;
+    }
+
+    decodeBasicToken(base64String: string){
+        const decoded = Buffer.from(base64String, 'base64').toString('utf-8');
+
+        const split = decoded.split(':');
+
+        if(split.length !== 2){
+            throw new UnauthorizedException('Invalid basic token');
+        }
+
+        const email = split[0];
+        const password = split[1];
+
+        return {email, password};
+
     }
 
 }
