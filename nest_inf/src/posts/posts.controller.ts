5640a2d6b96c63347acd1ca6514da57c901d6470
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-05-08 00:11:11 +0900

    feat: Parse Int Pipe

--- nest_inf/src/posts/posts.controller.ts
+++ nest_inf/src/posts/posts.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Param, Put } from '@nestjs/common';
+import { Controller, Get, Param, Put, ParseIntPipe } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getPosts(){
+    return this.postsService.getAllPosts()
+  }
+
+
+  @Get(':id')
+  getPost(@Param('id', ParseIntPipe) id: string){
+    return this.postsService.getPostById(+id)
+  }
 
   @Put(':id')
   putPost(
