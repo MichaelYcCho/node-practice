d9d57a2da8fd03810607298934652703361062fa
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-05-31 00:00:37 +0900

    feat: ImageUpload Temp EndPoint

--- nest_inf/src/common/common.module.ts
+++ nest_inf/src/common/common.module.ts
@@ -1,9 +1,45 @@
 import { BadRequestException, Module } from '@nestjs/common'
 import { CommonService } from './common.service'
+import * as multer from 'multer'
+import { TEMP_FOLDER_PATH } from 'src/common/const/path.const'
+import { v4 as uuid } from 'uuid'
+import { MulterModule } from '@nestjs/platform-express'
+import { extname } from 'path'
+import { CommonController } from './common.controller'
+import { AuthModule } from 'src/auth/auth.module'
+import { UsersModule } from 'src/users/users.module'
 
 @Module({
-    imports: [],
-    controllers: [],
+    imports: [
+        AuthModule,
+        UsersModule,
+        MulterModule.register({
+            limits: {
+                // 바이트 단위로 입력(10MB)
+                fileSize: 1024 * 1024 * 10,
+            },
+            fileFilter: (req, file, cb) => {
+                // callback(에러, boolean) -> 첫번째 파라미터는 에러가 있을경우 에러정보를, 두번째 파라미터는 파일을 받을지 여부를 boolean으로 전달
+                const ext = extname(file.originalname)
+
+                if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
+                    return cb(new BadRequestException('jpg/jpeg/png 파일만 업로드 가능합니다!'), false)
+                }
+
+                return cb(null, true)
+            },
+            storage: multer.diskStorage({
+                destination: function (req, res, cb) {
+                    cb(null, TEMP_FOLDER_PATH)
+                },
+                filename: function (req, file, cb) {
+                    // xxx.png
+                    cb(null, `${uuid()}${extname(file.originalname)}`)
+                },
+            }),
+        }),
+    ],
+    controllers: [CommonController],
     providers: [CommonService],
     exports: [CommonService],
 })
--- nest_inf/src/common/const/path.const.ts
+++ nest_inf/src/common/const/path.const.ts
@@ -4,6 +4,7 @@ export const PROJECT_ROOT_PATH = process.cwd()
 export const PUBLIC_FOLDER_NAME = 'public'
 export const POSTS_FOLDER_NAME = 'posts'
 export const USERS_FOLDER_NAME = 'users'
+export const TEMP_FOLDER_NAME = 'temp'
 
 // 공개폴더 절대 경로
 export const PUBLIC_FOLDER_PATH = join(PROJECT_ROOT_PATH, PUBLIC_FOLDER_NAME)
@@ -13,3 +14,6 @@ export const POST_IMAGE_PATH = join(PUBLIC_FOLDER_PATH, POSTS_FOLDER_NAME)
 
 // /public/posts/xxx.jpg
 export const POST_PUBLIC_IMAGE_PATH = join(PUBLIC_FOLDER_NAME, POSTS_FOLDER_NAME)
+
+// {프로젝트경로}/temp
+export const TEMP_FOLDER_PATH = join(PUBLIC_FOLDER_PATH, TEMP_FOLDER_NAME)
