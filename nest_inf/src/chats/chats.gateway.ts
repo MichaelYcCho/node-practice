90beba07b1b29eb8bfd395545953c09c1e1b3553
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-06-15 00:00:11 +0900

    feat: Server Event Emit

--- nest_inf/src/chats/chats.gateway.ts
+++ nest_inf/src/chats/chats.gateway.ts
@@ -1,5 +1,11 @@
-import { MessageBody, OnGatewayConnection, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets'
-import { Socket } from 'socket.io'
+import {
+    MessageBody,
+    OnGatewayConnection,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+} from '@nestjs/websockets'
+import { Server, Socket } from 'socket.io'
 
 @WebSocketGateway({
     namespace: 'chats', // -> ws://localhost:3000/chats
@@ -9,6 +15,9 @@ import { Socket } from 'socket.io'
     },
 })
 export class ChatsGateway implements OnGatewayConnection {
+    @WebSocketServer()
+    server: Server
+
     handleConnection(socket: Socket) {
         console.log(`on connection: ${socket.id}`)
     }
@@ -16,6 +25,6 @@ export class ChatsGateway implements OnGatewayConnection {
     // socket.on('send_message', 'hello')
     @SubscribeMessage('send_message')
     sendMessage(@MessageBody() message: string) {
-        console.log(message)
+        this.server.emit('receive_message', 'hello from server')
     }
 }
