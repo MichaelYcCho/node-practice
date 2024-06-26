a1b2ea4377b9209e2a7d669cb998045a0cf2e28d
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-06-27 00:00:11 +0900

    feat: Post Patch only Author

--- nest_inf/src/posts/posts.controller.ts
+++ nest_inf/src/posts/posts.controller.ts
@@ -32,6 +32,7 @@ import { HttpExceptionFilter } from 'src/common/exception-filter/http.exception-
 import { Roles } from 'src/users/decorator/roles.decorator'
 import { RolesEnum } from 'src/users/const/roles.const'
 import { IsPublic } from 'src/common/decorator/is-public.decorator'
+import { IsPostMineOrAdminGuard } from './guard/is-post-mine-or-admin.guard'
 
 @Controller('posts')
 export class PostsController {
@@ -84,7 +85,8 @@ export class PostsController {
     }
 
     @Patch(':id')
-    patchPost(@Param('id') id: string, @Body() body: UpdatePostDto) {
+    @UseGuards(IsPostMineOrAdminGuard)
+    patchPost(@Param('postId', ParseIntPipe) id: number, @Body() body: UpdatePostDto) {
         return this.postsService.updatePost(+id, body)
     }
 
